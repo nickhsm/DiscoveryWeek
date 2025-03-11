c230cfd48b087b8bb2e7aaaef009c26e5df8921a
nickhsm <r0938000@student.thomasmore.be>
2025-03-11 14:49:47 +0100

    Start met processen na het drukken op een knop, nog probleem met functie.

--- audio.py
+++ audio.py
@@ -1,8 +1,18 @@
 import streamlit as st
-
+import audio_processing
 
 def audio():
     '''
     Page that used for audio recognition.
     '''
     st.title("Audio herkenning")
+    
+    with st.form(key="test"):
+        audio_path = st.file_uploader("Upload hier de audio bestand",
+                                      type=["wav"]
+                                      )
+        submit_button = st.form_submit_button(label="Start")
+
+    if submit_button:
+        results = audio_processing.audio_process(audio_path)
+        st.write(results)
